332f4cb48fd449d572eb0f282ca74b71451bcdbb
zhang zihang <129568431+goodday1024@users.noreply.github.com>
2023-05-23 21:54:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -67,3 +67,65 @@ function addErrorMessage(message) {
         chatBody.appendChild(errorMessage);
         chatBody.scrollTop = chatBody.scrollHeight; // 自动滚动到底部
 }
+function sendImage() {
+        
+        var userInput = document.getElementById("user-input").value;
+        if (userInput !== "" && con.length <= 20) {
+          addUserImage(userInput)
+        }
+        else{
+          alert("内容为空或者已达上限")
+          return false
+        }
+        document.getElementById("seed").disabled = true;
+        document.getElementById("user-input").value = "";
+        
+        $.ajax({
+          url: "https://oa.api2d.net/v1/chat/completions",
+          type: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer fk202577-stNwU8r2VJSLlcJkOPqvWBmIaAK7NhiS",
+          },
+          data: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: con,
+          }),
+          success: function (data) {
+            console.log(data);
+            addBotImage(data[0].url);
+            console.log("success");
+            document.getElementById("seed").disabled = false;
+          },
+          error: function (data) {
+            console.log(data);
+            document.getElementById("seed").disabled = false;
+            addErrorMessage("生成失败，请重试");
+          },
+        });
+}
+function addUserText(message) {
+        var chatBody = document.getElementById("chat-body");
+        var userMessage = document.createElement("div");
+        userMessage.classList.add("chat-message", "user-message");
+        userMessage.textContent = message;
+        chatBody.appendChild(userMessage);
+        chatBody.scrollTop = chatBody.scrollHeight; // 自动滚动到底部
+}
+function addBotImage(message) {
+        var chatBody = document.getElementById("chat-body");
+        var botMessage = document.createElement("div");
+        botMessage.classList.add("chat-message", "bot-message");
+        var Image.src = message
+        botMessage.appendChild(Image);
+        chatBody.appendChild(botMessage);
+        chatBody.scrollTop = chatBody.scrollHeight; // 自动滚动到底部
+}
+function addErrorImage(message) {
+        var chatBody = document.getElementById("chat-body");
+        var errorMessage = document.createElement("div");
+        errorMessage.classList.add("chat-message", "error-message");
+        errorMessage.textContent = message;
+        chatBody.appendChild(errorMessage);
+        chatBody.scrollTop = chatBody.scrollHeight; // 自动滚动到底部
+}
