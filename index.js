5d37fbbc4301f21020e4b6587bfc73b123d82e7c
zhang zihang <129568431+goodday1024@users.noreply.github.com>
2023-05-23 22:28:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function sendImage() {
           }),
           success: function (data) {
             console.log(data);
-            addBotImage(data[0]);
+            addBotImage(data.data[0]);
             console.log("success");
             document.getElementById("seed").disabled = false;
           },
