f1b4200eb5c3f9dcfa165932fe9199ddc011d3c2
zhang zihang <129568431+goodday1024@users.noreply.github.com>
2023-05-25 21:04:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ function sendMessage() {
   }
   else {
     alert("内容为空或者已达上限")
+    if (con.length <= 20 && localStorage.getItem("sk") == null){
+      pay()
+    }
     return false
   }
   document.getElementById("seed").disabled = true;
